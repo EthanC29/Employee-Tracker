5bcb5d2d30111ff554da71032e83edf5df5fd8e9
Ethan <90936828+EthanC29@users.noreply.github.com>
2022-01-16 22:34:59 -0500

    moved everything to a single file, no more lib file or contents

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
-const { addDepartment } = require('./lib/addDepartment');
-const { addEmployee, updateEmployeeRole, updateEmployeeManager } = require('./lib/addEmployee');
-const { addRole } = require('./lib/addRole');
-const { viewDepartments } = require('./lib/viewDepartment');
-const { viewEmployees, viewEmployeesByManager } = require('./lib/viewEmployee');
-const { viewRoles } = require('./lib/viewRole');
 
 // Connect to database
 const db = mysql.createConnection(
@@ -22,48 +16,390 @@ const db = mysql.createConnection(
 
 const interfaceStart = () => {
   return inquirer
-  .prompt(
-    {
-      type: 'list',
-      name: 'initiationQuestion',
-      message: 'What would you like to do?',
-      choices: [
-        'View Employee',
-        'View Role',
-        'View Department',
-        'View Employees by Manager',
-        'Add Employee',
-        'Add Role',
-        'Add Department',
-        'Update Employee Role',
-        'Update Employee Manager',
-        'Delete Employee',
-        'Delete Role',
-        'Delete Department'
-      ]
-    }
-  )
-  .then(answer => {
-    if (answer === 'View Employee') {
-      viewEmployees();
-    } else if (answer === 'View Role') {
-      viewRoles();
-    } else if (answer === 'View Department') {
-      viewDepartments();
-    } else if (answer === 'View Employees by Manager') {
-      viewEmployeesByManager();
-    } else if (answer === 'Add Employee') {
-      addEmployee();
-    } else if (answer === 'Add Role') {
-      addRole();
-    } else if (answer === 'Add Department') {
-      addDepartment();
-    } else if (answer === 'Update Employee Role') {
-      updateEmployeeRole();
-    } else if (answer === 'Update Employee Manager') {
-      updateEmployeeManager();
-    }
-  })
+    .prompt([
+      {
+        type: 'list',
+        name: 'initiationQuestion',
+        message: 'What would you like to do?',
+        choices: [
+          'View Employee',
+          'View Role',
+          'View Department',
+          'View Employees by Manager',
+          'Add Employee',
+          'Add Role',
+          'Add Department',
+          'Update Employee Role',
+          'Update Employee Manager'
+        ]
+      }
+    ])
+    .then(answers => {
+      const answer = answers.initiationQuestion;
+      if (answer == 'View Employee') {
+        viewEmployee();
+      } else if (answer == 'View Role') {
+        viewRole();
+      } else if (answer == 'View Department') {
+        viewDepartment();
+      } else if (answer == 'View Employees by Manager') {
+        viewEmployeeByManager();
+      } else if (answer == 'Add Employee') {
+        addEmployee();
+      } else if (answer == 'Add Role') {
+        addRole();
+      } else if (answer == 'Add Department') {
+        addDepartment();
+      } else if (answer == 'Update Employee Role') {
+        updateEmployeeRole();
+      } else if (answer == 'Update Employee Manager') {
+        updateEmployeeManager();
+      }
+    });
 };
+
+// continue more actions
+const continueQuestion = () => {
+  return inquirer
+    .prompt(
+      {
+        type: 'list',
+        name: 'continue',
+        message: 'Continue?',
+        choices: [
+          'Yes',
+          'No (Quit)'
+        ]
+      }
+    )
+    .then(answers => {
+      const answer = answers.continue;
+      if (answer == 'Yes') {
+        interfaceStart();
+      } else {
+        process.exit();
+      }
+    });
+};
+
+// ----------------------------------  VIEW  ----------------------------------
+// display department table
+const viewDepartment = () => {
+  db.query('SELECT * FROM department;', (err, res) => {
+    if (err) {
+      console.log(err);
+    }
+    console.table(res);
+    continueQuestion();
+  })
+}
+
+// display role table
+const viewRole = () => {
+  db.query(`SELECT employee_role.*,
+              department.department_name AS department_name
+            FROM employee_role
+            LEFT JOIN department ON employee_role.department_id = department.id;`, (err, res) => {
+    if (err) {
+      console.log(err);
+    }
+    console.table(res);
+    continueQuestion();
+  })
+}
+
+// display employee table
+const viewEmployee = () => {
+  db.query(`SELECT employee.id, employee.first_name, employee.last_name, employee_role.title,
+              employee_role.salary, CONCAT(manager.first_name, ' ' , manager.last_name) AS manager
+            FROM employee
+            LEFT JOIN employee manager ON employee.manager_id = manager.id
+            INNER JOIN employee_role ON employee.role_id = employee_role.id;`, (err, res) => {
+    if (err) {
+      console.log(err);
+    }
+    console.table(res);
+    continueQuestion();
+  })
+}
+
+// display employee table ordered by manager
+const viewEmployeeByManager = () => {
+  db.query(`SELECT employee.id, employee.first_name, employee.last_name, employee_role.title,
+              employee_role.salary, CONCAT(manager.first_name, ' ' , manager.last_name) AS manager
+            FROM employee
+            LEFT JOIN employee manager ON employee.manager_id = manager.id
+            INNER JOIN employee_role ON employee.role_id = employee_role.id
+            ORDER BY manager;`, (err, res) => {
+    if (err) {
+      console.log(err);
+    }
+    console.table(res);
+    continueQuestion();
+  })
+}
+
+// -----------------------------------  ADD  ---------------------------------
+// add department
+const addDepartment = () => {
+  return inquirer
+    .prompt(
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Please enter the name of the new department:',
+        validate: nameInput => {
+          if (nameInput.length <= 30) {
+            return true;
+          } else {
+            console.log('Please enter no more than 30 characters');
+            return false;
+          };
+        }
+      }
+    )
+    .then(answer => {
+      db.query(`INSERT INTO department (department_name) VALUES ('${answer.name}');`, (err, res) => {
+        if (err) {
+          console.log(err);
+        }
+        viewDepartment();
+      });
+    });
+};
+
+// add role
+const addRole = () => {
+
+  db.query("SELECT id, department_name FROM department ORDER BY department_name;", (err, res) => {
+    if (err) {
+      console.log(err);
+    }
+    const departmentIdArray = res.map((departments) => {
+      return {
+        name: departments.department_name,
+        value: departments.id,
+      };
+    });
+
+    return inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'title',
+          message: 'Please enter the title of the new role:',
+          validate: titleInput => {
+            if (titleInput.length <= 30) {
+              return true;
+            } else {
+              console.log('Please enter no more than 30 characters');
+              return false;
+            };
+          }
+        },
+        {
+          type: 'input',
+          name: 'salary',
+          message: 'Please enter the salary of the new role (without decimals, currency signs, commas, or spaces):',
+          validate: salaryInput => {
+            if (salaryInput % 1 == 0) {
+              return true;
+            } else {
+              console.log('Please enter a whole number');
+              return false;
+            };
+          }
+        },
+        {
+          type: 'list',
+          name: 'departmentId',
+          message: "Select this role's department:",
+          choices: departmentIdArray
+        }
+      ])
+      .then(answers => {
+        db.query(`INSERT INTO employee_role (title, salary, department_id) VALUES ('${answers.title}', ${answers.salary}, ${answers.departmentId});`, (err, res) => {
+          if (err) {
+            console.log(err);
+          }
+          viewRole();
+        });
+      });
+  });
+};
+
+// add employee
+const addEmployee = () => {
+
+  db.query('SELECT id, title FROM employee_role ORDER BY title;', (err, res) => {
+    if (err) {
+      console.log(err);
+    }
+    const roleIdArray = res.map((roles) => {
+      return {
+        name: roles.title,
+        value: roles.id,
+      };
+    });
+
+    db.query('SELECT id, first_name, last_name FROM employee ORDER BY first_name;', (err, res) => {
+      if (err) {
+        console.log(err);
+      }
+      const managerIdArray = res.map((managers) => {
+        return {
+          name: managers.first_name + " " + managers.last_name,
+          value: managers.id,
+        };
+      });
+
+      return inquirer
+        .prompt([
+          {
+            type: 'input',
+            name: 'firstName',
+            message: 'Please enter the first name of the new employee:',
+            validate: firstNameInput => {
+              if (firstNameInput.length <= 30) {
+                return true;
+              } else {
+                console.log('Please enter no more than 30 characters');
+                return false;
+              };
+            }
+          },
+          {
+            type: 'input',
+            name: 'lastName',
+            message: 'Please enter the last name of the new employee:',
+            validate: lastNameInput => {
+              if (lastNameInput.length <= 30) {
+                return true;
+              } else {
+                console.log('Please enter no more than 30 characters');
+                return false;
+              };
+            }
+          },
+          {
+            type: 'list',
+            name: 'roleId',
+            message: "Select this employee's role:",
+            choices: roleIdArray
+          },
+          {
+            type: 'list',
+            name: 'managerId',
+            message: "Select this employee's supervisor:",
+            choices: managerIdArray
+          }
+        ])
+        .then(answers => {
+          db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('${answers.firstName}', '${answers.lastName}', ${answers.roleId}, ${answers.managerId});`, (err, res) => {
+            if (err) {
+              console.log(err);
+            }
+            viewEmployee();
+          });
+        });
+    });
+  });
+};
+
+// ---------------------------------------------------------------------------
+// update employee role
+const updateEmployeeRole = () => {
+
+  db.query('SELECT id, title FROM employee_role ORDER BY title;', (err, res) => {
+    if (err) {
+      console.log(err);
+    }
+    const roleArray = res.map((roles) => {
+      return {
+        name: roles.title,
+        value: roles.id,
+      };
+    });
+
+    db.query('SELECT id, first_name, last_name FROM employee ORDER BY first_name;', (err, res) => {
+      if (err) {
+        console.log(err);
+      }
+      const employeeArray = res.map((employees) => {
+        return {
+          name: employees.first_name + " " + employees.last_name,
+          value: employees.id,
+        };
+      });
+
+      return inquirer
+        .prompt([
+          {
+            type: 'list',
+            name: 'employeeId',
+            message: "Select the employee to be updated:",
+            choices: employeeArray
+          },
+          {
+            type: 'list',
+            name: 'roleId',
+            message: "Select this employee's new role:",
+            choices: roleArray
+          }
+        ])
+        .then(answers => {
+          db.query(`UPDATE employee SET role_id = ${answers.roleId} WHERE id = ${answers.employeeId};`, (err, res) => {
+            if (err) {
+              console.log(err);
+            }
+            viewEmployee();
+          });
+        });
+    });
+  });
+};
+
+// update employee manager
+const updateEmployeeManager = () => {
+
+  db.query('SELECT id, first_name, last_name FROM employee ORDER BY first_name;', (err, res) => {
+    if (err) {
+      console.log(err);
+    }
+    const employeeArray = res.map((employees) => {
+      return {
+        name: employees.first_name + " " + employees.last_name,
+        value: employees.id,
+      };
+    });
+    const managerArray = employeeArray.concat({ name: 'None', value: '' });
+
+    return inquirer
+      .prompt([
+        {
+          type: 'list',
+          name: 'employeeId',
+          message: "Select the employee to be updated:",
+          choices: employeeArray
+        },
+        {
+          type: 'list',
+          name: 'managerId',
+          message: "Select this employee's new manager:",
+          choices: managerArray
+        }
+      ])
+      .then(answers => {
+        db.query(`UPDATE employee SET manager_id = ${answers.managerId} WHERE id = ${answers.employeeId};`, (err, res) => {
+          if (err) {
+            console.log(err);
+          }
+          viewEmployee();
+        });
+      });
+  });
+};
+
+// ---------------------------------------------------------------------------
 
 interfaceStart();
